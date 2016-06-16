d419bfaccfde7429fb8618a09e47b729274116fc
Sawan J. Kapai Harpalani <sawankapai@gmail.com>
2016-06-16 17:37:50 +0100

    Added automated python script generator and scraper agent

---
+++ dataScraper/DSAgent.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# Title: DSAgent.py
+# Description: Scraping agent that takes an API as input and produces csv files of raw data
+# Author: Sawan J. Kapai Harpalani
+# Date	: 2016-06-16
+# Version: 0.1
+# Usage: python p.py
+# Notes:
+# python_version: 2.6.6
+# License: Copyright 200X Sawan J. Kapai Harpalani 
+# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify 
+# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, 
+# either version 3 of the License,  or (at your option) any later version 
+# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General 
+# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. 
+# If not, seehttp://www.gnu.org/licenses/.
+#=============================================================================
+
+
+import sys, getopt
+
+
+
+# Error Constants
+OPTION_ERROR = 0
+PARAM_ERROR = 1
+NO_ARG = 2
+MAND_ARG_MISSING = 3
+READING_CONF_ERROR = 4
+
+
+def main(argv):
+	if len(argv):
+		printError(NO_ARG)
+
+	try:
+		opts, args = getopt.getopt(argv, 'h:c:u:p', ['help', 'configFile=', 'username=', 'password='])
+	except getopt.GetoptError:
+		printError(OPTION_ERROR)
+
+	configFile = ''
+
+	for opt, arg in opts:
+		if opt in ('-h', '--help'):
+		 	printHelp()
+		elif opt in ('-c', '--config'):
+		 	configFile = arg
+		elif opt in ('-u', '--username'):
+		 	userName = arg
+		elif opt in ('-p', '--password'):
+		 	password = arg		
+		else:
+		 	printError(PARAM_ERROR)
+
+
+# Prints errors 
+def printError(errorType):
+	if errorType == OPTION_ERROR:
+		print 'Options Error: One or more options inserted do not exist'
+	elif errorType == PARAM_ERROR:
+		print 'Parameters Error: One or more parameters are missing'	
+	elif errorType == NO_ARG:
+		print 'No arguments shutting down! Bye Bye!'
+	elif errorType == MAND_ARG_MISSING:
+		print 'Mandatory arguments are missing!'
+	elif errorType == READING_CONF_ERROR:
+		print 'Error occurred while reading configuration file!'	
+
+	sys.exit(2)
---
+++ help/pythonHeader.py
@@ -0,0 +1,55 @@
+# Import the modules needed to run the script.
+from os.path import exists
+from time import strftime
+import os
+
+title = raw_input("Enter a title for your script: ")
+
+# Add .py to the end of the script.
+title = title + '.py'
+
+# Convert all letters to lower case.
+title = title.lower()
+
+# Remove spaces from the title.
+title = title.replace(' ', '_')
+
+# Check to see if the file exists to not overwrite it.
+if exists(title):
+    print "\nA script with this name already exists."
+    exit(1)
+
+descrpt = raw_input("Enter a description: ")
+name = "Sawan J. Kapai Harpalani"
+ver = "0.1"
+gnu = 'Copyright 200X Sawan J. Kapai Harpalani \n# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify \n# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, \n# either version 3 of the License,  or (at your option) any later version \n# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without \n# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General \n# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. \n# If not, seehttp://www.gnu.org/licenses/.'
+div = '======================================='
+
+# Create a file that can be written to.
+filename = open(title, 'w')
+
+# Set the date automatically.
+date = strftime("%Y-%m-%d")
+
+# Write the data to the file. 
+filename.write('#!/usr/bin/python')
+filename.write('\n# Title: ' + title)
+filename.write('\n# Description: ' + descrpt)
+filename.write('\n# Author: ' + name)
+filename.write('\n# Date: ' + date)
+filename.write('\n# Version: ' + ver)
+filename.write('\n# Usage: ' + 'python ' + title)
+filename.write('\n# Notes: ')
+filename.write('\n# python_version: 2.6.6')
+filename.write('\n# License: ' + gnu)
+filename.write('\n#' + div * 2 + '\n')
+filename.write('\n')
+filename.write('\n')
+
+# Close the file after writing to it.
+filename.close()
+
+# Clear the screen. This line of code will not work on Windows.
+os.system("clear") 
+
+print "Done!"
