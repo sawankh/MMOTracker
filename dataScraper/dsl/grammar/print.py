7c1850abfb5fd8fe756a07b9004ab56036d292b2
Sawan J. Kapai Harpalani <sawankapai@gmail.com>
2016-06-23 20:22:37 +0100

    Finally implemented mechanism for print

--- dataScraper/dsl/grammar/print.py
+++ dataScraper/dsl/grammar/print.py
@@ -17,4 +17,39 @@
 # If not, seehttp://www.gnu.org/licenses/.
 #==============================================================================
 
+from pyparsing import *
 
+from variable import *
+
+# Rules
+printReservedWord = Suppress(Literal("printConsole"))
+leftBracket = Suppress(Literal("("))
+rightBracket = Suppress(Literal(")"))
+number = Word(nums)
+message = QuotedString('"')
+variableID = identifier
+plus = Suppress(Literal("+"))
+printExpr = printReservedWord + leftBracket + (message.setResultsName("message", listAllMatches=True) | variableID.setResultsName("varID", listAllMatches=True)) + Optional(ZeroOrMore(plus + (message.setResultsName("message", listAllMatches=True) | variableID.setResultsName("varID", listAllMatches=True)))) + rightBracket
+
+stack = []
+v = assignment.parseString("a -> 2")
+stack.append(v)
+v = assignment.parseString("c -> 3")
+stack.append(v)
+x = printExpr.parseString("printConsole(a + \" sdwws \"+ c)")
+print x.dump()
+
+# Prints console parsed object
+def printConsole(parsedObject, varStack):
+	stringToPrint = ''
+	for element in parsedObject:
+		if element in parsedObject.varID.asList():
+			for item in varStack:
+				if element == item.varName:
+					stringToPrint += item.varValue
+		elif element in parsedObject.message.asList():
+			stringToPrint += element
+
+	print stringToPrint
+
+printConsole(x, stack)
