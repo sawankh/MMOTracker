bfe7c1b179eba3916f159862ad9cab36342384bd
Sawan J. Kapai Harpalani <sawankapai@gmail.com>
2016-06-23 17:36:20 +0100

    Added print script

---
+++ dataScraper/dsl/grammar/print.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Title: print.py
+# Description: Contains the gramma or value on the console
+# Author: Sawan J. Kapai Harpalani
+# Date: 2016-06-23
+# Version: 0.1
+# Usage: python print.py
+# Notes: 
+# python_version: 2.6.6
+# License: Copyright 200X Sawan J. Kapai Harpalani 
+# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify 
+# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, 
+# either version 3 of the License,  or (at your option) any later version 
+# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General 
+# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. 
+# If not, seehttp://www.gnu.org/licenses/.
+#==============================================================================
+
+
