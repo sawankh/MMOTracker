f33a785bc68ea5fdf7b309653ab439b8c1def08c
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-06-28 23:29:41 +0100

    Implemented parse string and parse file methods

--- dataScraper/dsl/scrapingDsl.py
+++ dataScraper/dsl/scrapingDsl.py
@@ -31,3 +31,11 @@ from grammar.behaviour.scrapeString import *
 # DSL types of expression
 newLine = Suppress(White("\n"))
 agentDSL = ZeroOrMore((scrapeURLExpr | scrapeStringExpr | clearExpr | commentsExpr | loopExpr.setParseAction(lambda tokens: loop(tokens, agentDSL)) | printExpr | sleepExpr | assignment) + Optional(newLine))
+
+# Parses an entire file
+def dslParseFile(fileName):
+	agentDSL.parseFile(fileName)
+
+# Parses a string
+def dslParseString(string):
+	agentDSL.parseString(string)
