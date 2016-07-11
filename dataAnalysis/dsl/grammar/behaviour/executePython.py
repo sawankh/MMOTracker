36677b511e9c2ccf6e5e3a3687717893b3ef7cfc
Sawan J. Kapai Harpalani <sawankapai@gmail.com>
2016-07-11 15:58:09 +0100

    Added grammar executePython

---
+++ dataAnalysis/dsl/grammar/behaviour/executePython.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# Title: executePython.py
+# Description: Contains the grammar and the methods to execute a Python script
+# Author: Sawan J. Kapai Harpalani
+# Date: 2016-07-11
+# Version: 0.1
+# Usage: python executePython.py
+# Notes: 
+# python_version: 2.6.6
+# License: Copyright 200X Sawan J. Kapai Harpalani 
+# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify 
+# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, 
+# either version 3 of the License,  or (at your option) any later version 
+# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General 
+# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. 
+# If not, seehttp://www.gnu.org/licenses/.
+#==============================================================================
+
+from pyparsing import *
+
+
+# Rules
+comma = Suppress(Literal(","))
+executePythonReservedWord = Suppress(Keyword("executePython"))
+fileName =  QuotedString('"', escChar = "\\").setResultsName("fileName")
+arguments = QuotedString('"', escChar = "\\").setResultsName("arguments", listAllMatches = True)
+leftBracket = Suppress(Literal("("))
+rightBracket = Suppress(Literal(")"))
+varID = Word(alphas, alphanums + "_").setResultsName("varID", listAllMatches = True)
+executePythonExpr = executePythonReservedWord + leftBracket + (fileName | varID) + ZeroOrMore(comma + (arguments | varID)) + rightBracket
