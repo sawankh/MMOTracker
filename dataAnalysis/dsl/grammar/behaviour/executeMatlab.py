084bc40e6d496dc0bf0b4ed9c7ae2fa8a38ccb3b
Sawan J. Kapai Harpalani <sawankapai@gmail.com>
2016-07-12 13:01:40 +0100

    Added matlab base script

---
+++ dataAnalysis/dsl/grammar/behaviour/executeMatlab.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# Title: executeMatlab.py
+# Description: Contains the grammar and the methods to execute a Matlab script
+# Author: Sawan J. Kapai Harpalani
+# Date: 2016-07-12
+# Version: 0.1
+# Usage: python executeMatlab.py
+# Notes: 
+# python_version: 2.6.6
+# License: Copyright 200X Sawan J. Kapai Harpalani 
+# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify 
+# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, 
+# either version 3 of the License,  or (at your option) any later version 
+# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General 
+# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. 
+# If not, seehttp://www.gnu.org/licenses/.
+#==============================================================================
+
+
