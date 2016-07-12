b9accf630a59afb13ce2b91837068a604460b1f3
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2016-07-13 00:58:47 +0100

    Added textlineNumbers class, for adding lines in text widget

---
+++ src/gui/textLineNumbers.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Title: textLineNumbers.py
+# Description: Contains the class to define text lines in a widget
+# Author: Sawan J. Kapai Harpalani
+# Date: 2016-07-13
+# Version: 0.1
+# Usage: python textLineNumbers.py
+# Notes: 
+# python_version: 2.6.6
+# License: Copyright 200X Sawan J. Kapai Harpalani 
+# This file is part of MMOTracker. MMOTracker is free software: you can redistribute it  and/or modify 
+# it under the terms of the GNU GeneralPublic License as published by the Free Software  Foundation, 
+# either version 3 of the License,  or (at your option) any later version 
+# MMOTracker is distributed in the hope that it willbe useful, but WITHOUT ANY WARRANTY; without 
+# even the implied warranty of MERCHANTABILITY or FITNESSFOR A PARTICULAR PURPOSE. See the GNU General 
+# PubliLicense for more details.You should have received a copy of the GNU GeneralPublic License along with MMOTracker. 
+# If not, seehttp://www.gnu.org/licenses/.
+#==============================================================================
+
+import Tkinter as tk
+
+class TextLineNumbers(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        tk.Canvas.__init__(self, *args, **kwargs)
+        self.textwidget = None
+
+    def attach(self, text_widget):
+        self.textwidget = text_widget
+
+    def redraw(self, *args):
+        '''redraw line numbers'''
+        self.delete("all")
+
+        i = self.textwidget.index("@0,0")
+        while True :
+            dline= self.textwidget.dlineinfo(i)
+            if dline is None: break
+            y = dline[1]
+            linenum = str(i).split(".")[0]
+            self.create_text(2,y,anchor="nw", text=linenum)
+            i = self.textwidget.index("%s+1line" % i)
